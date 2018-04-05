19bf3480e09dd73fd07c8f4cfa21d04c0e082ae8
Neel Patel <neel.patel.2012.np@gmail.com>
2018-04-05 23:24:34 +0530

    configure layouts.

--- app/src/main/java/tk/vn/app/presentation/fragments/CourseViewActivityLearningFragment.java
+++ app/src/main/java/tk/vn/app/presentation/fragments/CourseViewActivityLearningFragment.java
@@ -1,16 +1,26 @@
 package tk.vn.app.presentation.fragments;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import tk.vn.app.R;
 import tk.vn.app.com.CardPlayProcessor;
 import tk.vn.app.com.Const;
+import tk.vn.app.com.RunTimeStore;
+import tk.vn.app.model.GoalBean;
+import tk.vn.app.presentation.adapters.CourseViewGoalListItemAdapter;
 
 /**
  * A fragment with a Google +1 button.
@@ -24,6 +34,7 @@ public class CourseViewActivityLearningFragment extends Fragment {
     }
 
     private String title;
+    private ListView listView;
 
 //    private CardPlayProcessor cardPlayProcessor;
 
@@ -44,6 +55,7 @@ public class CourseViewActivityLearningFragment extends Fragment {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -51,7 +63,20 @@ public class CourseViewActivityLearningFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_course_view_activity_learning, container, false);
 
         TextView headerText = view.findViewById(R.id.header_text);
+        listView = view.findViewById(R.id.list_view);
         headerText.setText(title);
+        CardPlayProcessor processor = RunTimeStore.get(Const.CARD_PROCESSOR);
+        if(processor == null){
+            processor = new CardPlayProcessor(RunTimeStore.get(Const.COURSE_DETAIL),
+                    RunTimeStore.get(Const.SUBSCRIPTION_DETAIL));
+            RunTimeStore.storeObj(Const.CARD_PROCESSOR,processor);
+        }
+//
+//        if(processor!=null){
+        List<GoalBean> goalBeans = processor.getLearningGoalPlays();
+        System.out.println(goalBeans);
+        inflateList(goalBeans);
+//        }
 
         return view;
     }
@@ -64,5 +89,16 @@ public class CourseViewActivityLearningFragment extends Fragment {
         // Refresh the state of the +1 button each time the activity receives focus.
     }
 
-
+    private void inflateList(List<GoalBean> list){
+        final CourseViewGoalListItemAdapter adapter =
+                new CourseViewGoalListItemAdapter(getContext(), list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                GoalBean goalBean = ((GoalBean)adapterView.getItemAtPosition(i));
+                Toast.makeText(getContext(),"goalId - "+goalBean.getGoalId(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
--- app/src/main/java/tk/vn/app/presentation/fragments/CourseViewActivityMasterFragment.java
+++ app/src/main/java/tk/vn/app/presentation/fragments/CourseViewActivityMasterFragment.java
@@ -1,16 +1,26 @@
 package tk.vn.app.presentation.fragments;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import tk.vn.app.R;
 import tk.vn.app.com.CardPlayProcessor;
 import tk.vn.app.com.Const;
+import tk.vn.app.com.RunTimeStore;
+import tk.vn.app.model.GoalBean;
+import tk.vn.app.presentation.adapters.CourseViewGoalListItemAdapter;
 
 /**
  * A fragment with a Google +1 button.
@@ -24,6 +34,7 @@ public class CourseViewActivityMasterFragment extends Fragment {
     }
 
     private String title;
+    private ListView listView;
 
 //    private CardPlayProcessor cardPlayProcessor;
 
@@ -44,6 +55,7 @@ public class CourseViewActivityMasterFragment extends Fragment {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -51,7 +63,20 @@ public class CourseViewActivityMasterFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_course_view_activity_master, container, false);
 
         TextView headerText = view.findViewById(R.id.header_text);
+        listView = view.findViewById(R.id.list_view);
         headerText.setText(title);
+        CardPlayProcessor processor = RunTimeStore.get(Const.CARD_PROCESSOR);
+        if(processor == null){
+            processor = new CardPlayProcessor(RunTimeStore.get(Const.COURSE_DETAIL),
+                    RunTimeStore.get(Const.SUBSCRIPTION_DETAIL));
+            RunTimeStore.storeObj(Const.CARD_PROCESSOR,processor);
+        }
+//
+//        if(processor!=null){
+        List<GoalBean> goalBeans = processor.getAllGoalPlays();
+        System.out.println(goalBeans);
+        inflateList(goalBeans);
+//        }
 
         return view;
     }
@@ -63,5 +88,17 @@ public class CourseViewActivityMasterFragment extends Fragment {
         // Refresh the state of the +1 button each time the activity receives focus.
     }
 
+    private void inflateList(List<GoalBean> list){
+        final CourseViewGoalListItemAdapter adapter =
+                new CourseViewGoalListItemAdapter(getContext(), list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                GoalBean goalBean = ((GoalBean)adapterView.getItemAtPosition(i));
+                Toast.makeText(getContext(),"goalId - "+goalBean.getGoalId(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 }
